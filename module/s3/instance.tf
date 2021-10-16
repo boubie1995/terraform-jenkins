7858f9f2792dab259dda66dfccbe9cd59f87f562
Boubie <diallo.b.boubacar95@gmail.com>
2021-10-16 12:06:31 -0400

    added resources

--- module/s3/instance.tf
+++ module/s3/instance.tf
@@ -1,8 +1,10 @@
 resource "aws_s3_bucket" "backup" {
   bucket = var.s3_bucket
-  # key    = var.s3_key_name
-  # source = var.s3_source
-
+  key    = var.s3_key_name
+  source = var.s3_source
+  shared_credentials_file = "/Users/boubie/.aws/credentials"
+  profile                 = "admin"
+}
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"

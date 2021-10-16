fa90cf3247233add06b9460ec1ec94d499c07f36
Boubie <diallo.b.boubacar95@gmail.com>
2021-10-16 12:10:10 -0400

    fix brace

--- module/s3/instance.tf
+++ module/s3/instance.tf
@@ -4,13 +4,14 @@ resource "aws_s3_bucket" "backup" {
   source = var.s3_source
   shared_credentials_file = "/Users/boubie/.aws/credentials"
   profile                 = "admin"
+  
+  tags = {
+    Name = "jenkinsTerraform-S3"
+  }
 }
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   # etag = filemd5(var.s3_source)
 
-  tags = {
-    Name = "jenkinsTerraform-S3"
-  }
-}
+  
